e9ce062e08828f619f79fdca6572d9d47fa06106
Chris Bain <baincd@users.noreply.github.com>
2021-03-11 21:35:57 -0500

    Implement diff colorization

--- src/extension.ts
+++ src/extension.ts
@@ -3,82 +3,167 @@ import * as vscode from 'vscode';
 // this method is called when vs code is activated
 export function activate(context: vscode.ExtensionContext) {
 
-	console.log('decorator sample is activated');
+	// console.log('diff-lang-colorizer activated');
 
-	let timeout: NodeJS.Timer | undefined = undefined;
+	// ***** Read config and manage text decorations *****
+	// https://code.visualstudio.com/api/references/vscode-api#DecorationRenderOptions
+	// https://code.visualstudio.com/api/references/vscode-api#DecorationOptions
+	let headerDecorationType: vscode.TextEditorDecorationType;
+	let rangeDecorationType: vscode.TextEditorDecorationType;
+	let extendedHeaderDecorationType: vscode.TextEditorDecorationType;
 
-	// create a decorator type that we use to decorate small numbers
-	const smallNumberDecorationType = vscode.window.createTextEditorDecorationType({
-		borderWidth: '1px',
-		borderStyle: 'solid',
-		overviewRulerColor: 'blue',
-		overviewRulerLane: vscode.OverviewRulerLane.Right,
-		light: {
-			// this color will be used in light color themes
-			borderColor: 'darkblue'
-		},
-		dark: {
-			// this color will be used in dark color themes
-			borderColor: 'lightblue'
-		}
-	});
+	function disposeAllTextDecorations() {
+		headerDecorationType?.dispose();
+		rangeDecorationType?.dispose();
+		extendedHeaderDecorationType?.dispose();
+	}
 
-	// create a decorator type that we use to decorate large numbers
-	const largeNumberDecorationType = vscode.window.createTextEditorDecorationType({
-		cursor: 'crosshair',
-		// use a themable color. See package.json for the declaration and default values.
-		backgroundColor: { id: 'myextension.largeNumberBackground' }
-	});
+	function handleUpdatedConfig() {
+		disposeAllTextDecorations();
+		let colorizerConfig = vscode.workspace.getConfiguration("diff-lang-colorizer");
 
-	let activeEditor = vscode.window.activeTextEditor;
+		headerDecorationType = vscode.window.createTextEditorDecorationType({
+			fontWeight: colorizerConfig.get<string>('header.fontWeight'),
+			fontStyle:  colorizerConfig.get<string>('header.fontStyle'),
+			light: {
+				color: colorizerConfig.get<string>('header.lightThemeForegroundColor')
+			},
+			dark: {
+				color: colorizerConfig.get<string>('header.darkThemeForegroundColor')
+			},
+		});
 
-	function updateDecorations() {
-		if (!activeEditor) {
+		rangeDecorationType = vscode.window.createTextEditorDecorationType({
+			fontWeight: colorizerConfig.get<string>('range.fontWeight'),
+			fontStyle:  colorizerConfig.get<string>('range.fontStyle'),
+			light: {
+				color: colorizerConfig.get<string>('range.lightThemeForegroundColor')
+			},
+			dark: {
+				color: colorizerConfig.get<string>('range.darkThemeForegroundColor')
+			},
+		});
+
+		extendedHeaderDecorationType = vscode.window.createTextEditorDecorationType({
+			fontWeight: colorizerConfig.get<string>('extendedHeader.fontWeight'),
+			fontStyle:  colorizerConfig.get<string>('extendedHeader.fontStyle'),
+			opacity: colorizerConfig.get<string>('extendedHeader.opacity')
+		});
+	}
+
+	handleUpdatedConfig();
+
+	// ***** Decorate diff Text Editors *****
+
+	function updateDecorationsOnAllVisibleEditors() {
+		vscode.window.visibleTextEditors.forEach(e => updateDecorationsOnEditor(e));
+	}
+
+	// Regexs based on https://github.com/microsoft/vscode/blob/main/extensions/git/syntaxes/diff.tmLanguage.json
+	// const HeaderGitRegEx = /^diff --git a\/.*$/; // Git RegEx isn't necessary - HeaderCmdRegEx will cover it
+	const HeaderCmdRegEx = /^diff (-|\S+\s+\S+).*$/
+	const rangeNormalRegEx = /^\d+(,\d+)*(a|d|c)\d+(,\d+)*$/
+	const rangeUnifiedRegEx = /^(@@)\s*(.+?)\s*(@@)/
+	const rangeContextRegEx = /^(((\-{3}) .+ (\-{4}))|((\*{3}) .+ (\*{4})))$/
+
+	function updateDecorationsOnEditor(editor: vscode.TextEditor | undefined) {
+		if (editor?.document.languageId != 'diff') {
 			return;
 		}
-		const regEx = /\d+/g;
-		const text = activeEditor.document.getText();
-		const smallNumbers: vscode.DecorationOptions[] = [];
-		const largeNumbers: vscode.DecorationOptions[] = [];
-		let match;
-		while ((match = regEx.exec(text))) {
-			const startPos = activeEditor.document.positionAt(match.index);
-			const endPos = activeEditor.document.positionAt(match.index + match[0].length);
-			const decoration = { range: new vscode.Range(startPos, endPos), hoverMessage: 'Number **' + match[0] + '**' };
-			if (match[0].length < 3) {
-				smallNumbers.push(decoration);
-			} else {
-				largeNumbers.push(decoration);
+
+		const headers: vscode.DecorationOptions[] = [];
+		const ranges: vscode.DecorationOptions[] = [];
+		const extHeaders: vscode.DecorationOptions[] = [];
+
+		let doc = editor.document;
+		let match: RegExpMatchArray | null = null;
+		let lineIdx;
+		let inExtHeader = false;
+		for (lineIdx = 0; lineIdx < doc.lineCount; lineIdx++) {
+			let line = doc.lineAt(lineIdx);
+			if (HeaderCmdRegEx.test(line.text)) {
+				headers.push({ range: line.range });
+				inExtHeader = true;
+			} else if ( (match = rangeUnifiedRegEx.exec(line.text))) {
+				ranges.push({ range: new vscode.Range(lineIdx, 0, lineIdx, match[0].length) });
+				inExtHeader = false;
+			} else if (rangeNormalRegEx.test(line.text) ||  rangeContextRegEx.test(line.text)) {
+				ranges.push({ range: line.range });
+				inExtHeader = false;
+			} else if (inExtHeader) {
+				extHeaders.push({ range: line.range });
 			}
 		}
-		activeEditor.setDecorations(smallNumberDecorationType, smallNumbers);
-		activeEditor.setDecorations(largeNumberDecorationType, largeNumbers);
+		editor.setDecorations(headerDecorationType, headers);
+		editor.setDecorations(rangeDecorationType, ranges);
+		editor.setDecorations(extendedHeaderDecorationType, extHeaders);
 	}
 
-	function triggerUpdateDecorations() {
-		if (timeout) {
-			clearTimeout(timeout);
-			timeout = undefined;
+	function clearDecorationsOnEditor(editor: vscode.TextEditor | undefined) {
+		editor?.setDecorations(headerDecorationType, []);
+		editor?.setDecorations(rangeDecorationType, []);
+		editor?.setDecorations(extendedHeaderDecorationType, []);
+	}
+
+	// ***** Trigger updates of text editors *****
+
+	let updateActiveEditorTimeout: NodeJS.Timer | undefined = undefined;
+	let updateAllEditorsTimeout: NodeJS.Timer | undefined = undefined;
+
+
+	function triggerUpdateActiveEditorDecorations() {
+		if (updateActiveEditorTimeout) {
+			clearTimeout(updateActiveEditorTimeout);
+			updateActiveEditorTimeout = undefined;
 		}
-		timeout = setTimeout(updateDecorations, 500);
+		updateActiveEditorTimeout = setTimeout(() => updateDecorationsOnEditor(vscode.window.activeTextEditor), 100);
 	}
 
-	if (activeEditor) {
-		triggerUpdateDecorations();
-	}
-
-	vscode.window.onDidChangeActiveTextEditor(editor => {
-		activeEditor = editor;
-		if (editor) {
-			triggerUpdateDecorations();
+	function triggerUpdateAllEditorsDecorations() {
+		if (updateAllEditorsTimeout) {
+			clearTimeout(updateAllEditorsTimeout);
+			updateAllEditorsTimeout = undefined;
 		}
+		updateAllEditorsTimeout = setTimeout(updateDecorationsOnAllVisibleEditors, 100);
+	}
+
+	triggerUpdateAllEditorsDecorations();
+
+	// ***** Listeners for interesting events *****
+
+	// vscode.window.onDidChangeActiveTextEditor(editor => {
+	// 	if (editor) {
+	// 		triggerUpdateActiveEditorDecorations();
+	// 	}
+	// }, null, context.subscriptions);
+
+	vscode.window.onDidChangeVisibleTextEditors(editors => {
+		triggerUpdateAllEditorsDecorations()
 	}, null, context.subscriptions);
 
 	vscode.workspace.onDidChangeTextDocument(event => {
-		if (activeEditor && event.document === activeEditor.document) {
-			triggerUpdateDecorations();
+		if (vscode.window.activeTextEditor && event.document === vscode.window.activeTextEditor.document) {
+			triggerUpdateActiveEditorDecorations();
 		}
 	}, null, context.subscriptions);
 
+	vscode.workspace.onDidOpenTextDocument(doc => {
+		if (doc.languageId != 'diff') {
+			clearDecorationsOnEditor(vscode.window.activeTextEditor);
+		} else {
+			triggerUpdateActiveEditorDecorations();
+		}
+	}, null, context.subscriptions)
+	
+	vscode.workspace.onDidChangeConfiguration(e => {
+		handleUpdatedConfig();
+		triggerUpdateAllEditorsDecorations();	
+	}, null, context.subscriptions)
+
+	// ***** Cleanup *****
+
+	context.subscriptions.push({
+		dispose: disposeAllTextDecorations
+	});
 }
 
