2ff159bedbdd1a7d9ce2df60fdb6e4ea567f4ba9
Chris Bain <baincd@users.noreply.github.com>
2021-03-18 22:26:28 -0400

    Remove unnecessary code following refactorings

--- src/CodeAndInvisibleLineBreakHighlighter.ts
+++ src/CodeAndInvisibleLineBreakHighlighter.ts
@@ -12,80 +12,12 @@ export function activate(context: vscode.ExtensionContext) {
 		vscode.window.visibleTextEditors.forEach(e => updateDecorationsOnEditor(e));
 	}
 
-	const fencedCodeBlockStartRegEx = /^\s{0,3}(`{3,}|~{3,})\s*(?=([^`~]*)?$)/ // Based on fenced_code_block_unknown (limiting preceding spaces to 3) "(^|\\G)(\\s*)(`{3,}|~{3,})\\s*(?=([^`~]*)?$)"
-	const fencedCodeBlockEndRegExStr = "^\\s{0,3}({MATCH1})\\s*$" // Based on fenced_code_block_unknown (limiting preceding spaces to 3) "(^|\\G)(\\2|\\s{0,3})(\\3)\\s*$"
-	
-	const indentedCodeBlockRegEx = /^([ ]{4}|\t)/; // Based on raw_block "(^|\\G)([ ]{4}|\\t)"
-
-	const headingRegEx = /^[ ]{0,3}(#{1,6}\s+(.*?)(\s+#{1,6})?\s*)$/
-	const endFencedCodeBlockRegEx = /^\s*(`{3,}|~{3,})\s*/
-	const blockQuoteRegEx = /^[ ]{0,3}(>) ?/
-
-
 	function updateDecorationsOnEditor(editor: vscode.TextEditor | undefined) {
 		if (editor?.document.languageId != 'markdown') {
 			return;
 		}
 
-
-		let doc = editor.document;
-		let match: RegExpMatchArray | null = null;5
-		let lineIdx = -1;
-		while (++lineIdx < doc.lineCount) {
-			let line = doc.lineAt(lineIdx);
-			if (match = line.text.match(fencedCodeBlockStartRegEx)) {
-				let startLine = lineIdx + 1;
-				if (startLine < doc.lineCount) { 
-					let fencedCodeBlockEndRegEx = new RegExp(fencedCodeBlockEndRegExStr.replace("{MATCH1}",match[1]));
-					let endLine = -1;
-					while (endLine == -1 && ++lineIdx < doc.lineCount) {
-						line = doc.lineAt(lineIdx);
-						if (line.text.match(fencedCodeBlockEndRegEx)) {
-							endLine = lineIdx - 1;
-						}
-					}
-					if (endLine == -1) {
-						endLine = doc.lineCount - 1;
-					}
-					if (startLine <= endLine) {
-					}
-				}
-			} else if (indentedCodeBlockRegEx.test(line.text)) {
-				let isCodeBlock: boolean;
-				if (lineIdx == 0) {
-					isCodeBlock = true;
-				} else {
-					let prevLineText = doc.lineAt(lineIdx - 1).text;
-					isCodeBlock = prevLineText.trim().length == 0 
-								|| headingRegEx.test(prevLineText)
-								|| endFencedCodeBlockRegEx.test(prevLineText)
-								|| blockQuoteRegEx.test(prevLineText);
-				}
-				if (isCodeBlock) {
-					let startLine = lineIdx;
-					let endOfBlockFound = false;
-					let endLine = startLine;
-					while (!endOfBlockFound && ++lineIdx < doc.lineCount) {
-						if (indentedCodeBlockRegEx.test(doc.lineAt(lineIdx).text)) {
-							endLine = lineIdx;
-						} else if (doc.lineAt(lineIdx).text.trim().length != 0) {
-							endOfBlockFound = true
-							lineIdx--; // set line index to previous line, so next time through loop evaluates this line
-						}	
-					}
-				}
-			} else {
-
-
-			}
-		}
-
-		
 		Decorator.updateDecorations(editor, null, {isCancellationRequested: false});
-	}
-
-	function setEditorDecorations(editor: vscode.TextEditor, config: ConfigurationHandler.ExtensionFeatureConfig, ranges: vscode.DecorationOptions[]) {
-		editor?.setDecorations(config.decorationType, (config.enabled ? ranges : []));
 	}
 
 	function clearDecorationsOnEditor(editor: vscode.TextEditor | undefined) {
